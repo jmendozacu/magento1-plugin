4f40c6fe642022eee46edbff32c4771408a8fff1
spsinghats <spsingh@athenasoft.in>
2019-10-12 19:11:55 +0530

    Create ceevo_script.js

---
+++ js/ceevo/ceevo_script.js
@@ -0,0 +1,40 @@
+$(document).on("click",".btn-checkout",function(event)
+{
+	
+	window.stop();
+	event.preventDefault();
+	var apikey =  document.getElementById('apikey').value;
+	var modde  = document.getElementById('paymode').value;
+	var price  = document.getElementById('idprice').value;
+	var curr  = document.getElementById('idcurr').value;
+	
+    var formId = '#co-payment-form';
+	  var config = {
+		envMode: modde, // LIVE
+		receiveTokensEvent: true // support custom event listener 'receiveTokens' to send tokenise data to merchant. default false.
+	  };
+
+	 var ceevoPayment = new CeevoPayment(apikey, formId, config);
+	 var widget = ceevoPayment.widget();
+     var amount = Math.round(price);
+     var amountTotal =  String(amount);
+     var currecny  = String(curr); 
+    // old
+		ceevoPayment.setPrice(amountTotal);
+		ceevoPayment.setCurrency(currecny);
+		ceevoPayment.open_widget();
+
+
+		 // listen custom event when receiveTokensEvent is true
+	    document.getElementById('co-payment-form').addEventListener('receiveTokens', function ({ detail }) {
+				//console.log('form.eventListener', detail);
+				//console.log("test--", detail.card_token)
+				document.getElementById("token_hidden_input").value = detail.card_token;
+				document.getElementById('session_hidden_input').value = detail.session_id;
+				document.getElementById('method_code').value = detail.method_code;
+				
+				review.save();		
+
+	    });   
+       
+ });
