1a37977a8cc56053e0d1d8bdc246af6012f098f6
Thomas Tai <thomastai@localadmins-mbp.ad.transact24.com>
2019-10-14 10:50:47 +0800

    chore: updated install script for the renaming of the module

--- install.sh
+++ install.sh
@@ -3,13 +3,16 @@
 
 WEBROOT=/var/www/html
 if [ ! -d "$WEBROOT/app/code/local/Mage" ]; then
-	mkdir $WEBROOT/app/code/local
-	mkdir $WEBROOT/app/code/local/Mage
+	mkdir -p $WEBROOT/app/code/local/Mage
 fi
-cp -r ./Mage/NewModule $WEBROOT/app/code/local/Mage/
-cp ./NewModule.xml $WEBROOT/app/etc/modules/
-if [ ! -d "$WEBROOT/app/design/frontend/base/default/template/NewModule" ]; then
-	mkdir $WEBROOT/app/design/frontend/base/default/template/NewModule
+cp -r ./Mage/CeevoPayment $WEBROOT/app/code/local/Mage/
+cp ./CeevoPayment.xml $WEBROOT/app/etc/modules/
+if [ ! -d "$WEBROOT/app/design/frontend/base/default/template/ceevopayment" ]; then
+	mkdir $WEBROOT/app/design/frontend/base/default/template/ceevopayment
 fi
-cp -r ./form $WEBROOT/app/design/frontend/base/default/template/NewModule/
-cp ./layout/newmodule.xml $WEBROOT/app/design/frontend/base/default/layout/NewModule.xml
+cp -r ./form $WEBROOT/app/design/frontend/base/default/template/ceevopayment/
+cp ./layout/ceevopayment.xml $WEBROOT/app/design/frontend/base/default/layout/
+if [ ! -d "$WEBROOT/js/ceevo" ]; then
+	mkdir $WEBROOT/js/ceevo
+fi
+cp ./ceevo_script.js $WEBROOT/js/ceevo/
