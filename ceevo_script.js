165aa36a4bf2c08cd0642ce980bf8080ab8e5e0c
spsinghats <spsingh@athenasoft.co.com>
2020-01-17 16:31:13 +0530

    file Formatting

--- ceevo_script.js
+++ ceevo_script.js
@@ -1,59 +1,44 @@
-$(document).on("click",".btn-checkout",function(event)
-{
-	if (getRadioBoxValue("payment[method]") != "ceevopayment"){
-		return;
-	}
-	window.stop();
-	event.preventDefault();
-	var apikey =  document.getElementById('apikey').value;
-	var modde  = document.getElementById('paymode').value;
-	var price  = document.getElementById('idprice').value;
-	var curr  = document.getElementById('idcurr').value;
-	
-    var formId = '#co-payment-form';
-	  var config = {
-		envMode: modde, // LIVE
-		receiveTokensEvent: true // support custom event listener 'receiveTokens' to send tokenise data to merchant. default false.
-	  };
-	  console.log("price="+price);
-	 var ceevoPayment = new CeevoPayment(apikey, formId, config);
-	//  var widget = ceevoPayment.widget();
-    //  var amount = Math.round(price);
-    //  var amountTotal =  String(amount);
-     var currecny  = String(curr); 
-	//  console.log("amount="+amount);
-        // if(document.querySelector('tr.last .price')){
-		// 	var totalAmount = document.querySelector('tr.last .price').innerHTML;
-		// 	totalAmount = totalAmount.replace(',', ".");
-		// 	// alert(totalAmount);
-        //     amount = totalAmount.replace(/[^0-9.-]+/g,"");
-        // } 
-        var total = parseFloat(price).toFixed(2);
-		console.log("total="+total);
-		ceevoPayment.setPrice(String(total));
-		ceevoPayment.setCurrency(currecny);
-		ceevoPayment.open_widget();
+$(document).on("click", ".btn-checkout", function(event) {
+  if (getRadioBoxValue("payment[method]") != "ceevopayment") {
+    return;
+  }
+  window.stop();
+  event.preventDefault();
+  var apikey = document.getElementById("apikey").value;
+  var modde = document.getElementById("paymode").value;
+  var price = document.getElementById("idprice").value;
+  var curr = document.getElementById("idcurr").value;
 
-		 // listen custom event when receiveTokensEvent is true
-	    document.getElementById('co-payment-form').addEventListener('receiveTokens', function ({ detail }) {
-				//console.log('form.eventListener', detail);
-				//console.log("test--", detail.card_token)
-				document.getElementById("token_hidden_input").value = detail.card_token;
-				document.getElementById('session_hidden_input').value = detail.session_id;
-				document.getElementById('method_code').value = detail.method_code;
-				
-				review.save();		
-	    });       
- });
+  var formId = "#co-payment-form";
+  var config = {
+    envMode: modde, // LIVE
+    receiveTokensEvent: true // support custom event listener 'receiveTokens' to send tokenise data to merchant. default false.
+  };
+  console.log("price=" + price);
+  var ceevoPayment = new CeevoPayment(apikey, formId, config);
+  var currecny = String(curr);
+   var total = parseFloat(price).toFixed(2);
+  console.log("total=" + total);
+  ceevoPayment.setPrice(String(total));
+  ceevoPayment.setCurrency(currecny);
+  ceevoPayment.open_widget();
 
- 
-function  getRadioBoxValue(radioName) 
-{ 
-      var obj = document.getElementsByName(radioName);
-         for(i=0; i<obj.length;i++)  {
-         if(obj[i].checked)  { 
-             return  obj[i].value; 
-         } 
-       }     
-       return "undefined";    
+  // listen custom event when receiveTokensEvent is true
+  document.getElementById("co-payment-form").addEventListener("receiveTokens", function({ detail }) {
+      document.getElementById("token_hidden_input").value = detail.card_token;
+      document.getElementById("session_hidden_input").value = detail.session_id;
+      document.getElementById("method_code").value = detail.method_code;
+
+      review.save();
+    });
+});
+
+function getRadioBoxValue(radioName) {
+  var obj = document.getElementsByName(radioName);
+  for (i = 0; i < obj.length; i++) {
+    if (obj[i].checked) {
+      return obj[i].value;
+    }
+  }
+  return "undefined";
 }
